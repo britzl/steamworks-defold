814341814d2d4b56c4464854e2626662577df022
Brian <subsoap@gmail.com>
2019-08-12 19:15:24 -0700

    Update run_osx.sh
    When was this changed? I don't know, but it's osx now.

--- run_osx.sh
+++ run_osx.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cp steamworks/bundle/osx/Contents/MacOS/* build/default
-cp build/x86_64-darwin/dmengine build/default
+cp build/x86_64-osx/dmengine build/default
 cp steam_appid.txt build/default
 cd build/default
 ./dmengine
