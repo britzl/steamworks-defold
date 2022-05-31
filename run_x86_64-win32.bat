6dc85443031fe612d967cc08519abde524840641
Dmitriy Pyrin <dmitrii.pyrin@luden.io>
2022-05-31 14:26:09 +0400

    windows batch fix

--- run_x86_64-win32.bat
+++ run_x86_64-win32.bat
@@ -1,6 +1,8 @@
 copy %~dp0\steamworks\res\x86_64-win32\steam_api64.dll %~dp0\build\default
 copy %~dp0\build\x86_64-win32\dmengine.exe %~dp0\build\default
+copy %~dp0\build\x86_64-win32\OpenAL32.dll %~dp0\build\default
+copy %~dp0\build\x86_64-win32\wrap_oal.dll %~dp0\build\default
 copy %~dp0\steam_appid.txt %~dp0\build\default
 cd %~dp0\build\default
-dmengine.exe
+dmengine.exe ./game.projectc 
 pause
