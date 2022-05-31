f90014f59f623dac226b2f7d8fbb94a573c7ca0e
Dmitriy Pyrin <dmitrii.pyrin@luden.io>
2022-05-31 15:39:12 +0400

    osx and linux batches fix

--- run_x86_64-linux.sh
+++ run_x86_64-linux.sh
@@ -4,4 +4,4 @@ cp steamworks/res/x86_64-linux/* build/default
 cp build/x86_64-linux/dmengine build/default
 cp steam_appid.txt build/default
 cd build/default
-./dmengine
+./dmengine ./game.projectc
